988340a1668a04763407aab924ffabe386961234
Vincent S. <vincent.silvestri@hotmail.fr>
2025-07-17 16:35:51 +0200

    correction

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-  output: "server",
   adapter: vercel(),
 });
