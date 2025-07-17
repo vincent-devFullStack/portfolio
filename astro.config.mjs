4807cc1b2bd5907d28440260c162feed66188298
Vincent S. <vincent.silvestri@hotmail.fr>
2025-07-17 17:00:05 +0200

    fix: meta SEO 404.astro + cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,9 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
+  site: "https://www.vince-dev.fr",
   adapter: vercel(),
+  integrations: [
+    /* ... */
+  ],
 });
